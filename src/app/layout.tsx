e776aedf577dce0b00e3c33fb5696b940c473746
Deepak Bhandari <bhandarideepakdev@gmail.com>
2026-05-30 03:59:56 +0300

    Wrap GoogleAnalytics in Suspense for prerender

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Suspense } from "react";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 import "./globals.css";
 
@@ -31,7 +32,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <GoogleAnalytics gaId={gaId} />
+        <Suspense fallback={null}>
+          <GoogleAnalytics gaId={gaId} />
+        </Suspense>
         {children}
       </body>
     </html>
